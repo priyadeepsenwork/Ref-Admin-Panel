83f3c61823473f1497b93452cda4a8e935a574a2
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-03-22 19:38:03 +0530

    SideBar Structure Done

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import Layout from "./components/layout";
+import { resources } from "./config/resources";
 
 const App = () => {
   return (
@@ -36,6 +37,7 @@ const App = () => {
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
               authProvider={authProvider}
+              resources={resources}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,22 +1,25 @@
 import {Avatar as AntdAvatar, AvatarProps} from 'antd'
+import { getNameInitials } from '../utilities'
 
 type Props = AvatarProps & {
-    name: string
+    name?: string
 }
 
 const CustomAvatar = ({ name, style, ...rest}: Props) => {
   return (
     <AntdAvatar
-    alt={`Priyadeep Sen`}
+    alt={name}
     size="small"
     style={{
         backgroundColor: '#87d068',
         display: 'flex',
         alignItems: 'center',
-        border: 'nonoe'
+        border: 'none',
+        ...style
     }}
+    {...rest}
     >
-        {name}
+        {getNameInitials(name || '')}
     </AntdAvatar>
   )
 }
