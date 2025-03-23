2cab1e543c0eef1fd598038e4ac5f72770c326d5
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-03-23 09:15:22 +0530

    displayUpcomingEvents-1

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import UpcomingEvents from "./Home/upcoming-events";
+import DealsChart from "./Home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+};
